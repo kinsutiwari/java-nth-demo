948f917500f334cd03992f4f5f48e3ab81d7d50f
Kinsu Tiwari <kinsu.bca@gmail.com>
2025-03-24 17:37:37 +0530

    1st commit

---
+++ src/main/java/com/kinsu/java/JavaDemoJava817Application.java
@@ -0,0 +1,13 @@
+package com.kinsu.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoJava817Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoJava817Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinsu/java/JavaDemoJava817ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinsu.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoJava817ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
